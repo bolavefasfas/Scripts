a02424c76116e7e899b6fd09d6f7ee56d956611a
bolavefasfas <82230241+bolavefasfas@users.noreply.github.com>
2022-09-24 04:21:09 +0530

    Update Script.sh

--- BasicTools/Script.sh
+++ BasicTools/Script.sh
@@ -6,23 +6,12 @@ sudo apt upgrade -y
 sudo apt autoremove -y
 echo "Installing Docker-Compose"
 sudo apt install docker-compose -y
-echo "Installing Rclone"
-sudo -v ; curl https://rclone.org/install.sh | sudo bash
-
-while true; do
-
-read -p "Do you want to proceed? (y/n) " yn
-
-case $yn in 
-	[yY] ) echo "Installing Cloudflare" && curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb && sudo dpkg -i cloudflared.deb;
-		break;;
-	[nN] ) echo exiting...;
-		exit;;
-	* ) echo invalid response;;
-esac
-
-done
-
-
 docker ps
 docker images
+echo "Installing Rclone"
+sudo -v ; curl https://rclone.org/install.sh | sudo bash
+rclone version
+#echo "Installing Cloudflare" 
+#curl -L --output cloudflared.deb 
+#https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb 
+#sudo dpkg -i cloudflared.deb
