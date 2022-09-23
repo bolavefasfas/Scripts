f6a8979308066a87059166ec924aab708482d506
bolavefasfas <82230241+bolavefasfas@users.noreply.github.com>
2022-09-24 04:15:48 +0530

    Update Script.sh

--- BasicTools/Script.sh
+++ BasicTools/Script.sh
@@ -9,14 +9,15 @@ sudo apt install docker-compose -y
 echo "Installing Rclone"
 sudo -v ; curl https://rclone.org/install.sh | sudo bash
 
-read -r -p "Do you want to Install Cloudflare Tunnels? [y/N] " response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
-then
-    echo "Installing Cloudflare"
-curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb 
-sudo dpkg -i cloudflared.deb
-else
-    rclone version
-fi
+read -p "Do you want to proceed? (yes/no) " yn
+
+case $yn in 
+	yes ) echo "Installing Cloudflare" && curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb && sudo dpkg -i cloudflared.deb;;
+	no ) echo exiting...;
+		exit;;
+	* ) echo invalid response;
+		exit 1;;
+esac
+
 docker ps
 docker images
